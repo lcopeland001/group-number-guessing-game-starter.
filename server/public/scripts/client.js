8024de1bf6dac8ca4d40f3c48ce13569052477c7
Latisha Copeland <lcopeland001@gmail.com>
2022-07-30 12:04:11 -0500

    post guesses

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,27 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  // event handlers
+  $('#submit-guesses-button').on('click', submitGuesses)
 }
+
+function submitGuesses() {
+  console.log('In submitGuesses')
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: {
+      guessOne: $('#guess-one').val(),
+      guessTwo: $('#guess-two').val()
+    }
+  }).then(function (response) {
+    //call a get route
+    // call guesses 
+    // call results
+    // append to DOM - <li> guessOne: resultOne looke like "Player one guessed 2 - that was high"
+  }).catch(function (error) {
+    console.log(error);
+    alert('Something went wrong oh nooooo!!!')
+  })
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = 5002;
+
+const guessArray = [];
+// can make a reset button that calls this function on a POST??
+let randomNumber = randomNumberGenerator()
+console.log(randomNumber);
+function randomNumberGenerator() {
+  return Math.floor(Math.random()*25)+1
+}
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -10,6 +18,36 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+
+app.post('/guesses', (req, res) =>{
+  const guess = req.body;
+  console.log(guess);
+  if(parseFloat(guess.guessOne) === randomNumber) {
+    firstGuessResult = 'Correct!'
+  } else if(guess.guessOne < randomNumber) {
+    firstGuessResult = 'Low'
+  } else if(guess.guessOne > randomNumber) {
+    firstGuessResult = 'High'
+  }
+  if(parseFloat(guess.guessTwo) === randomNumber) {
+    secondGuessResult = 'Correct!'
+  } else if(guess.guessTwo < randomNumber) {
+    secondGuessResult = 'Low'
+  } else if(guess.guessTwo > randomNumber) {
+    secondGuessResult = 'High'
+  }
+  guess.resultOne = firstGuessResult
+  guess.resultTwo = secondGuessResult
+  guessArray.push(guess);
+  console.log(guess)
+  res.sendStatus(201); 
+})
+// guess {
+//  guessOne: value from client
+//  guessTwo: value from client
+//  resultOne: high/low/correct
+//  resultTwo: high/low/correct
+// }
 
 
 app.listen(PORT, () => {
